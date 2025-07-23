96c95774529ef12dfe119d9674922065051fbc53
Braian A. Diez <bdiez19@gmail.com>
2025-07-23 18:53:33 -0300

    feat(update): currently update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import UnoCSS from "@unocss/astro";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  integrations: [
-    UnoCSS({ injectReset: true }),
-    mdx({
-      syntaxHighlight: 'shiki',
-      gfm: true,
-    }),
-  ],
-  site: "https://kaneru.netlify.app/",
-  markdown: {
-    gfm: true,
-  },
+	integrations: [
+		UnoCSS({ injectReset: true }),
+		mdx({
+			syntaxHighlight: "shiki",
+			gfm: true,
+		}),
+	],
+	site: "https://kaneru.netlify.app/",
+	markdown: {
+		gfm: true,
+	},
 });
--- uno.config.ts
+++ uno.config.ts
@@ -1,25 +1,22 @@
 import { defineConfig, presetTypography, presetWind3 } from "unocss";
 
 export default defineConfig({
-  presets: [
-    presetWind3(),
-    presetTypography(),
-  ],
-  theme: {
-    fontFamily: {
-      mono: "GeistMono",
-      jp: "Tsunagi",
-      alt: "Schabo",
-    },
-    colors: {
-      "jp-red-deep": "#b3131a",
-      "jp-red-bright": "#ff2229",
-      "jp-blue": "#212475",
-      "jp-purple": "#9691D4",
-      "jp-dark-primary": "#090b0e",
-      "jp-dark-secondary": "#1d1b1c",
-      "jp-white": "#ffffff",
-      "jp-cream": "#dfd6af",
-    },
-  },
+	presets: [presetWind3(), presetTypography()],
+	theme: {
+		fontFamily: {
+			mono: "GeistMono",
+			jp: "Tsunagi",
+			alt: "Schabo",
+		},
+		colors: {
+			"jp-red-deep": "#b3131a",
+			"jp-red-bright": "#ff2229",
+			"jp-blue": "#212475",
+			"jp-purple": "#9691D4",
+			"jp-dark-primary": "#090b0e",
+			"jp-dark-secondary": "#1d1b1c",
+			"jp-white": "#ffffff",
+			"jp-cream": "#dfd6af",
+		},
+	},
 });
